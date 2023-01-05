7e9441a4f7798d31b901d870b1b16d9b2b2c7967
smartywebdev0119 <smartwebdev0119@gmail.com>
2023-01-05 15:15:59 +0530

    Service Discovery & Registration with Eureka

--- section8/accounts/src/main/java/com/eazybytes/accounts/model/Accounts.java
+++ section8/accounts/src/main/java/com/eazybytes/accounts/model/Accounts.java
@@ -2,9 +2,9 @@ package com.eazybytes.accounts.model;
 
 import java.time.LocalDate;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 
 import lombok.Getter;
 import lombok.Setter;
