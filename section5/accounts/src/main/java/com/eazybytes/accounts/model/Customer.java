767a5fef5b851883b5fe902fe498b191984e0923
smartywebdev0119 <smartwebdev0119@gmail.com>
2023-01-05 10:41:27 +0530

    Making microservices Docker compatible

--- section5/accounts/src/main/java/com/eazybytes/accounts/model/Customer.java
+++ section5/accounts/src/main/java/com/eazybytes/accounts/model/Customer.java
@@ -2,11 +2,11 @@ package com.eazybytes.accounts.model;
 
 import java.time.LocalDate;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
 import lombok.Getter;
 import lombok.Setter;
