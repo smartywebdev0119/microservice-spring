9790954b7bedb5ca7f604391ab07d7c66c76e70c
smartywebdev0119 <smartwebdev0119@gmail.com>
2023-01-23 12:40:05 +0530

    Auto Scaling, Healing & Deployment using Kubernetes

--- section_13/gatewayserver/src/main/java/com/eaztbytes/gatewayserver/GatewayserverApplication.java
+++ section_13/gatewayserver/src/main/java/com/eaztbytes/gatewayserver/GatewayserverApplication.java
@@ -2,11 +2,20 @@ package com.eaztbytes.gatewayserver;
 
 import java.util.Date;
 
+import com.eaztbytes.gatewayserver.trace.logging.ObservationContextSnapshotLifter;
+import io.micrometer.context.ContextSnapshot;
+import io.micrometer.tracing.Tracer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.ApplicationListener;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.event.ContextRefreshedEvent;
+import reactor.core.publisher.Hooks;
+import reactor.core.publisher.Operators;
 
 @SpringBootApplication
 public class GatewayserverApplication {
@@ -33,6 +42,15 @@ public class GatewayserverApplication {
 		            .filters(f -> f.rewritePath("/eazybank/cards/(?<segment>.*)","/${segment}")
 		            		.addResponseHeader("X-Response-Time",new Date().toString()))
 		            .uri("lb://CARDS")).build();
+	}
+
+	@ConditionalOnClass({ContextSnapshot.class, Hooks.class})
+	@ConditionalOnWebApplication(type = ConditionalOnWebApplication.Type.REACTIVE)
+	@Bean
+	public ApplicationListener<ContextRefreshedEvent> reactiveObservableHook() {
+		return event -> Hooks.onEachOperator(
+				ObservationContextSnapshotLifter.class.getSimpleName(),
+				Operators.lift(ObservationContextSnapshotLifter.lifter()));
 	}
 
 }
