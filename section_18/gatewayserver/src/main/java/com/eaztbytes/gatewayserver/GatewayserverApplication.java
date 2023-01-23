7eb17f11472246d2ca6b00813410b80d2bf1c3a7
smartywebdev0119 <smartwebdev0119@gmail.com>
2023-01-23 13:25:43 +0530

    Securing Microservices using OAuth2 Authorization code grant flow

--- section_18/gatewayserver/src/main/java/com/eaztbytes/gatewayserver/GatewayserverApplication.java
+++ section_18/gatewayserver/src/main/java/com/eaztbytes/gatewayserver/GatewayserverApplication.java
@@ -3,12 +3,21 @@ package com.eaztbytes.gatewayserver;
 import java.util.Date;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import com.eaztbytes.gatewayserver.trace.logging.ObservationContextSnapshotLifter;
+import io.micrometer.context.ContextSnapshot;
+import io.micrometer.tracing.Tracer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.gateway.filter.factory.TokenRelayGatewayFilterFactory;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.ApplicationListener;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.event.ContextRefreshedEvent;
+import reactor.core.publisher.Hooks;
+import reactor.core.publisher.Operators;
 
 @SpringBootApplication
 public class GatewayserverApplication {
@@ -41,6 +50,15 @@ public class GatewayserverApplication {
 							.rewritePath("/eazybank/cards/(?<segment>.*)","/${segment}")
 							.removeRequestHeader("Cookie"))
 		            .uri("lb://CARDS")).build();
+	}
+
+	@ConditionalOnClass({ContextSnapshot.class, Hooks.class})
+	@ConditionalOnWebApplication(type = ConditionalOnWebApplication.Type.REACTIVE)
+	@Bean
+	public ApplicationListener<ContextRefreshedEvent> reactiveObservableHook() {
+		return event -> Hooks.onEachOperator(
+				ObservationContextSnapshotLifter.class.getSimpleName(),
+				Operators.lift(ObservationContextSnapshotLifter.lifter()));
 	}
 
 }
